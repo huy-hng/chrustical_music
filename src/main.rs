8995ac2db26fb60df6104898082374e19b077924
malet <malet@invalid>
2020-08-15 15:10:01 +0200

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use cpal::traits::HostTrait;
 
 fn main() {
-	let host = cpal::default_host();
-	let device = host
-		.default_input_device()
-		.expect("no output device available");
+    let host = cpal::default_host();
+    let device = host
+        .default_input_device()
+        .expect("no output device available");
 }
