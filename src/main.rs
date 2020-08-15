540f9e6ae91f2c6f47049c049bae62c13f45d7f4
Huy <huy@hhoang.dev>
2020-08-15 15:35:02 +0200

    print input device name

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use cpal::traits::HostTrait;
+use cpal::traits::{DeviceTrait, HostTrait};
 
 fn main() {
 	let host = cpal::default_host();
 	let device = host
 		.default_input_device()
-		.expect("no output device available");
+		.expect("Failed to get default input device");
+	println!("Default input device: {}", device.name().expect("no name"));
 }
