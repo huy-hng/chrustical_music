b6ada94c5a35051a8d3051ca2bb49bc613e91360
malet <malet@invalid>
2020-08-17 16:17:33 +0200

    Starting messyness

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,52 @@
+use std::sync::mpsc::{Receiver};
+
 pub struct BeatMessage {
 	pub bpm: f64,
 	pub since_last: usize,
 }
 
-pub static sample_rate: usize = 44100;
+pub async fn detect_beats(samples: Receiver<i16>, sample_rate: usize) -> Vec<BeatMessage> {
+	let mut max = 0usize;
+	let mut last = 0usize;
 
-pub fn detect_beats(track: Vec<i16>) -> Vec<BeatMessage> {
-	let avg: f64 = track
-		.iter()
-		.take(sample_rate)
-		.map(|x| *x as f64 / sample_rate as f64)
-		.sum();
-	let max = track.iter().max().cloned().unwrap_or_else(|| 0i16);
-	let mut last = 0;
-	track
+	// Check for overflows in enumerate and abs
+	samples
 		.iter()
 		.enumerate()
-		.map(|(i, x)| {
-			if *x >= max {
-				let lastlast = last;
-				last = i;
-				Some(BeatMessage {
-					bpm: 60f64 / (i - lastlast) as f64 * sample_rate as f64,
-					since_last: lastlast,
-				})
-			} else {
-				None
-			}
-		})
-		.filter_map(|x| x)
-		.collect()
+		.map(|(i, s)| {
+		if s.abs() as usize > max {
+			max = s.abs() as usize
+		}
+		if s.abs() as usize >= max {
+			let lastlast = last;
+			last = i;
+			Some(BeatMessage {
+				bpm: 60f64 / (i - lastlast) as f64 * sample_rate as f64,
+				since_last: lastlast,
+			})
+		} else {
+			None
+		}
+	}).collect::<Option<_>>().unwrap()
+
+	// let track = (0..sample_rate).map(|| samples.recv()).collect();
+	// let max = track.iter().max().cloned().unwrap_or_else(|| 0i16);
+	// let mut last = 0;
+	// track
+	// 	.iter()
+	// 	.enumerate()
+	// 	.map(|(i, x)| {
+	// 		if *x >= max {
+	// 			let lastlast = last;
+	// 			last = i;
+	// 			Some(BeatMessage {
+	// 				bpm: 60f64 / (i - lastlast) as f64 * sample_rate as f64,
+	// 				since_last: lastlast,
+	// 			})
+	// 		} else {
+	// 			None
+	// 		}
+	// 	})
+	// 	.filter_map(|x| x)
+	// 	.collect()
 }
